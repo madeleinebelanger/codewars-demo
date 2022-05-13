615792162854183545f4998f8d3fffce27f31fb0
m <b@Madeleine.local>
2022-05-13 14:27:48 -0700

    soloution 003 complete

--- js/soloution002.js
+++ js/soloution002.js
@@ -7,20 +7,23 @@
 
 // celsius = (fahrenheit - 32) * (5/9)
 // Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
+
+
 let theTemp= ''
 function convertToCelsius (temperature) {
   var celcius = (temperature) - 32 + (5/9)
-  theTemp.concat(celcius)
+  theTemp= theTemp+celcius
+  console.log(theTemp)
+
 }
 
-console.log(theTemp)
 
 function weatherInfo (theTemp) {
-  if (theTemp > 0)
+  if (theTemp < 0)
     return (theTemp + " is freezing temperature")
   else
     return (theTemp + " is above freezing temperature")
 }
 
-console.log(weatherInfo(12))
+console.log(weatherInfo(theTemp))
 
