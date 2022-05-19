9fd7287d07a31ca0dc62f50093d5191d6704a75a
Madeleine Belanger <102709523+madeleinebelanger@users.noreply.github.com>
2022-05-18 18:36:56 -0700

    Update soloution 030.js

--- js/soloution 030.js
+++ js/soloution 030.js
@@ -24,17 +24,6 @@ switch (expr){
 
 }
 
-}
+} 
 
 console.log(getDrinkByProfession("scHOOl counselor"))
-
-// Complete the function that receives as input a string, and produces outputs according to the following table:
-
-// Input	Output
-// "Jabroni"	"Patron Tequila"
-// "School Counselor"	"Anything with Alcohol"
-// "Programmer"	"Hipster Craft Beer"
-// "Bike Gang Member"	"Moonshine"
-// "Politician"	"Your tax dollars"
-// "Rapper"	"Cristal"
-// anything else	"Beer"
