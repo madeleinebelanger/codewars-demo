5a06aa819b4b695c9628df8f2d17050e43821f05
m <b@Madeleine.local>
2022-05-18 18:50:51 -0700

    Soloution 031 complete

---
+++ js/Soloution 031.js
@@ -0,0 +1,12 @@
+function remove (string) {
+string=string.split('')
+let check= string[string.length-1]
+let newArr= string.slice(0,-1)
+if(check=== "!") {
+return newArr.join('')}
+else{return string.join('')}
+
+
+}
+
+console.log( remove("madie"))
