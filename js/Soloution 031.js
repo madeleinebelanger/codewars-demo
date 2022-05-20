dda68440821853729147bc0748138360a167c665
m <b@Madeleine.local>
2022-05-19 21:01:11 -0700

    Soloution 033 completed

--- js/Soloution 031.js
+++ js/Soloution 031.js
@@ -1,12 +1,3 @@
-function remove (string) {
-string=string.split('')
-let check= string[string.length-1]
-let newArr= string.slice(0,-1)
-if(check=== "!") {
-return newArr.join('')}
-else{return string.join('')}
-
-
+function usdcny(usd) {
+  
 }
-
-console.log( remove("madie"))
