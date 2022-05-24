61b1fd73cff382efe2ac0d8e3507b5027823be4a
m <b@Madeleine.local>
2022-05-23 18:10:36 -0700

    Soloution 044 completed

---
+++ js/soloution 044.js
@@ -0,0 +1,15 @@
+function  calculateAge(yearOfBirth,howOldIsHe) {
+let hisAge= howOldIsHe-yearOfBirth
+
+if (hisAge==1){
+    return `You are ${hisAge} year old`}
+if(hisAge==-1){
+    return `You will be born in ${Math.abs(hisAge)} year.`}
+if(hisAge>0){
+    return `You are ${hisAge} years old`}
+if(hisAge<0){
+    return `You will be born in ${Math.abs(hisAge)} years.`}
+else{
+    return `You were born this very year!`}
+
+}
