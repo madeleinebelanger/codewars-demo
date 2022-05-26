bd45c664aa4d1c849dbe913226f7aa491443bba1
m <b@Madeleine.local>
2022-05-25 21:58:05 -0700

    Soloution 047 Completed

---
+++ js/Soloution 046.js
@@ -0,0 +1,12 @@
+
+
+function apple(x){
+x=Number(x)
+if( (x*x)>1000){
+return "It's hotter than the sun!!"
+}
+else{
+return 'Help yourself to a honeycomb Yorkie for the glovebox.'}
+}
+
+console.log(apple())
---
+++ js/Soloution 047.js
@@ -0,0 +1,10 @@
+function sayHello( name, city, state ) {
+name= name.join(' ')
+
+return `Hello, ${name}! Welcome to ${city}, ${state}!`
+
+
+
+}
+
+console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'))
