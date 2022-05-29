1aafa1eeea755a98cd88250096df9980fea04fd4
m <b@Madeleine.local>
2022-05-29 11:14:21 -0700

    Soloution 058 Complete

---
+++ js/Soloution 058.js
@@ -0,0 +1,15 @@
+function openOrSenior(data){
+let emptyArr=[]
+let test= data.map(member=> (member[0]>54)&& (member[1]>7) )
+
+for(let i=0; i<data.length;i++){
+    if(test[i]==true){emptyArr.push( "Senior")}
+    else {emptyArr.push( "Open")}}
+ 
+return emptyArr
+}
+
+
+
+
+console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]))
