cb71c5319bc5d6c29e79dcc0ee01f0b1bd752e69
m <b@Madeleine.local>
2022-05-30 08:39:28 -0700

    Soloution 060 Completed

---
+++ js/Soloution 060.js
@@ -0,0 +1,14 @@
+function SeriesSum(n){
+let sum= -2
+let emptyArr= []
+for(let i=1; i<=n; i++)
+        {emptyArr.push(sum+=3)}
+let percentages = emptyArr.map(num=> 1/num)
+let answer= percentages.reduce((a,b)=> a+b,0)
+return answer.toFixed(2)
+
+}
+
+
+
+console.log( SeriesSum())
