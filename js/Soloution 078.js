8f4df17381c41b6a7cc89092999e0276bfb9fe3c
m <b@Madeleine.local>
2022-06-04 10:16:16 -0700

    Soloution 078 complete

---
+++ js/Soloution 078.js
@@ -0,0 +1,19 @@
+function divisors(integer) {
+let emptyArr=[]
+for(let i=2 ; i<integer; i++){
+if(integer%i==0){emptyArr.push(i)}
+}
+if(emptyArr.length==0){
+return `${integer} is prime`
+}
+
+else{
+return emptyArr
+
+}
+
+
+};
+
+
+console.log(divisors(12))
