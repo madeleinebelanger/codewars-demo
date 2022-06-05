3107cf1a5c50b74059971f7b947cfb2c8f014463
m <b@Madeleine.local>
2022-06-05 09:25:39 -0700

    Soloution 080 complete

---
+++ js/Soloution 079.js
@@ -0,0 +1,16 @@
+
+function anki(arr){
+let emptyArr=[]
+for (let i =0; i<arr.length; i++) {
+if( arr[i]%2==0) { emptyArr.push(i)}
+emptyArr
+
+}
+
+
+}
+
+
+
+console.log(anki(["Keep", "Remove", "Keep", "Remove", "Keep"]
+))
---
+++ js/Soloution 080.js
@@ -0,0 +1,11 @@
+function breakChocolate(n,m) {
+if(n==0||m==0)
+{return 0}
+
+let totalBreaks= m*n - 1
+return totalBreaks
+
+}
+
+
+console.log(breakChocolate(1,1))
