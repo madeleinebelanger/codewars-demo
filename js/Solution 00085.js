8243f7dcd1a46f212cef63e48efd136d1242576a
m <b@Madeleine.local>
2022-06-13 11:34:02 -0700

    Solution 00085 complete

---
+++ js/Solution 00085.js
@@ -0,0 +1,17 @@
+function whoIsPaying(name){
+let emptyArr=[]
+let nameLength= name.length
+if(nameLength<= 2 || nameLength==0){
+emptyArr.push(name)
+}
+
+else{
+emptyArr.push(name)
+emptyArr.push(name.slice(0,2))
+}
+
+return emptyArr
+}
+
+
+console.log(whoIsPaying("maaa"))
