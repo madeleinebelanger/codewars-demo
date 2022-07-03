0752d8641db7164f944120a21b75cd2b47552a00
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-07-02 19:47:30 -0700

    Solution 00209 complete

--- Solution 00209.js
+++ Solution 00209.js
@@ -1,13 +1,9 @@
-class Dinglemouse{
+function sortMyString(S) {
+S=S.split('')
+let theEvens= S.filter((letters,index)=>index%2==0 ).join('')
+let theOdds=S.filter((letters,index)=>index%2!==0 ).join('')
+return theEvens + " "+theOdds
 
-  constructor( firstName, lastName ){
-    this.firstName=firstName
-    this.lastName=lastName
-
-  }
-  
-  getFullName(){
-    return firstName+" "+lastName
-  }
-  
 }
+
+console.log(sortMyString("YCOLUE'VREER"))
