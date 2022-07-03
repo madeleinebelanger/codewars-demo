dd7a92da9437e1bd99b59b72e459d94f50be5983
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-07-03 10:38:22 -0700

    Solution 00215 complete

---
+++ Solution 00215.js
@@ -0,0 +1,25 @@
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1+a2+d2;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return B1+e1+e1;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return b1+a1+n1+a1+n1+a1
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  return "yes"
+}
+function answer2(){
+ return "no"
+}
+function answer3(){
+ return "yes"
+}
+
