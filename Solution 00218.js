cb58361c34fc5f465a0be85575c4d11517890606
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-07-08 09:22:45 -0700

    Solution 00218 complete

--- Solution 00218.js
+++ Solution 00218.js
@@ -1,5 +1,6 @@
-function isSantaClausable(obj) {
-  return ['sayHoHoHo', 'distributeGifts', 'goDownTheChimney'].every(function(methodName) {
-    return typeof obj[methodName] == 'function';
-  });
-} 
+function leo(oscar){
+return oscar==88? "Leo finally won the oscar! Leo is happy": oscar==86? "Not even for Wolf of wallstreet?!": oscar<=87? "When will you give Leo an Oscar?": "Leo got one already!"
+
+}
+
+console.log( leo(86))
