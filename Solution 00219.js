863d806eab8ef32afda35e35c7711c2b19539ec3
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-07-12 17:10:39 -0700

    Solution 00219.js

--- Solution 00219.js
+++ Solution 00219.js
@@ -3,33 +3,48 @@ class Animal {
     this.name = name;
     this.age = age;
     this.legs = legs;
-    this.status = status;
     this.species = species;
+    this.status = status;
+
   }
   introduce(name,age,status) {
 
-    return `Hello, my name is ${this.name} and I am ${this.age} years old. My status is ${this.status}`;
+    return `Hello, my name is ${this.name} and I am ${this.age} years old. `;
   }
 }
 
 class Shark extends Animal {
-  constructor(name, status, age) {
-    super(name,status,age);
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
   }
-    
-
-
 }
 
 class Cat extends Animal {
-  // Do the same here as you did for Shark - define your constructor function and any other methods you need
+    constructor(name,age,status){
+    super(name,age,4,"cat",status)
+}
+introduce(name,age,status){
+return   `Hello, my name is ${this.name} and I am ${this.age} years old. Meow meow!`
+
+}
+
+
 }
 
 class Dog extends Animal {
-  // On your own now - you can do it :D
+constructor(name,age,status,master){
+super(name,age,4,"dog",status)
+this.master=master
+
 }
 
-let sharky= new Animal ("Sharky",10000,'Happy boi')
+  introduce(name,age,status) {
 
-console.log(sharky.introduce())
+    return `Hello, my name is ${this.name} and I am ${this.age} years old. `;
+  }
 
+ greetMaster(){
+    return `Hello ${this.master}`
+}
+
+}
